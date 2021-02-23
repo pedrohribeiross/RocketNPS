0612ee687fbe018b67a51099d4662346a14b5be1
Pedro Henrique <pedro_henrique.rss@hotmail.com>
2021-02-23 13:38:00 -0300

    Conclusão do segundo dia do NLW 4 - Rocketseat, onde criamos o Banco de Dados.

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
+
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-//  http://localhost:3333/
-app.get('/', (request, response) => {
-	return response.json({ message: 'Hello World - NLW 4' });
-});
-
-app.post('/', (request, response) => {
-	// Recebeu os dados para salvar
-	return response.json({ message: 'Os dados foram salvos com sucesso!' });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running'));
